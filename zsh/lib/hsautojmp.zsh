49d4a46022b0855d00ba8b2470345e4e376e1c82
Steffen <steffen.siering@gmail.com>
2010-07-24 03:33:27 +0200

    [zsh] replace autojump with hsautojump

---
+++ zsh/lib/hsautojmp.zsh
@@ -0,0 +1,15 @@
+
+HS_AUTOJUMP_HOME="$HOME/.cabal/"
+export PATH="$AUTOJUMP_HOME/bin:$PATH"
+
+function autojmp_preexec() {
+    { (hsautojmp add "$(pwd -P)"&)>/dev/null 2>>|${HOME}/.autojmp_errors ; } 2>/dev/null
+}
+
+typeset -ga preexec_functions
+preexec_functions+=autojmp_preexec
+
+alias jumpstat="hsautojmp stats"
+
+function j { local new_path="$(hsautojmp match $*)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";fi }
+
